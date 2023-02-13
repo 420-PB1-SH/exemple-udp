22e74ff1d9c36f597c37545faa9fd3867215283f
Pier-Luc Brault <pier-luc@brault.me>
2023-02-13 13:10:48 -0500

    Add code for client and server

--- Client/client-main.cpp
+++ Client/client-main.cpp
@@ -5,5 +5,60 @@ using namespace std;
 
 int main()
 {
-	system("pause");
+    sf::UdpSocket socket;
+    sf::SocketSelector selecteur;
+    sf::IpAddress adresseServeur = "127.0.0.1";
+    sf::IpAddress adressePaquetEntrant;
+    sf::Packet paquetEntrant;
+    sf::Packet paquetSortant;
+
+    unsigned short portServeur = 54000;
+    unsigned short portClient;
+    unsigned short portPaquetEntrant;
+
+    string message;
+
+    setlocale(LC_ALL, "fr");
+
+    // Attacher le socket à un port
+    // AnyPort indique d'utiliser un port assigné par le système
+    if (socket.bind(sf::Socket::AnyPort) != sf::Socket::Done) {
+        cout << "Une erreur est survenue lors de la création du socket." << endl;
+        return 1;
+    } 
+    portClient = socket.getLocalPort();
+
+    // On associe le socket à un sélecteur.
+    // Celui-ci permettra de mettre un temps d'attente maximal sur la réception de données.
+    selecteur.add(socket);
+
+    cout << "Écho Réseau" << endl;
+    cout << "===========" << endl << endl;
+
+    cout << "Le client écoute sur le port " << portClient << "." << endl << endl;
+
+    // Tant que le programme n'est pas arrêté par l'utilisateur
+    while (true) {
+        cout << "Entrer un message: ";
+        getline(cin, message);
+        paquetSortant << message;
+
+        socket.send(paquetSortant, adresseServeur, portServeur);
+
+        // On attend la réception de données sur le socket pour un maximum de 5 secondes.
+        // Passé ce délai, la méthode `wait` retournera false.
+        if (selecteur.wait(sf::seconds(5))) {
+            socket.receive(paquetEntrant, adressePaquetEntrant, portPaquetEntrant);
+
+            paquetEntrant >> message;
+
+            cout << "Le serveur (" << adressePaquetEntrant << ":" << portPaquetEntrant
+                << ") a retourné : " << message << endl;
+        }
+        else {
+            cout << "Le serveur n'a retourné aucune réponse après 5 secondes." << endl;
+        }
+
+        paquetSortant.clear(); // On efface le contenu de paquetSortant pour pouvoir le réutiliser
+    }
 }
--- Server/server-main.cpp
+++ Server/server-main.cpp
@@ -5,5 +5,40 @@ using namespace std;
 
 int main()
 {
-	system("pause");
+    sf::UdpSocket socket;
+    sf::IpAddress adresseClient;
+    sf::Packet paquetEntrant;
+    sf::Packet paquetSortant;
+
+    unsigned short portServeur = 54000;
+    unsigned short portClient;
+
+    string message;
+
+    setlocale(LC_ALL, "fr");
+
+    // Attacher le socket au port du serveur
+    if (socket.bind(portServeur) != sf::Socket::Done) {
+        cout << "Une erreur est survenue lors de la création du socket." << endl;
+        return 1;
+    }
+
+    cout << "Le serveur écoute sur le port " << portServeur << "." << endl;
+
+    // Tant que le programme n'est pas arrêté par l'utilisateur
+    while (true) {
+        // socket.receive est bloquant.
+        // L'exécution ne continuera pas tant que des données ne sont pas reçues.
+        socket.receive(paquetEntrant, adresseClient, portClient);
+
+        paquetEntrant >> message;
+
+        cout << adresseClient << " a envoyé: " << message << endl;
+
+        // Renvoyer le même message au client
+        paquetSortant << message;
+        socket.send(paquetSortant, adresseClient, portClient);
+
+        paquetSortant.clear(); // On efface le contenu de paquetSortant pour pouvoir le réutiliser
+    }
 }
